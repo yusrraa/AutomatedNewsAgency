5e197f381371431f1e019cec24083a0571a43bd4
Shiza Khan <sohailshiza56@gmail.com>
2022-05-06 11:34:37 +0500

    Front-end UI

--- adm_interface/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdmInterfaceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adm_interface'
--- autonewsagn/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import *
-
-def main(request):
-    return HttpResponse("<h1>Main Home page for user</h1>""This is the main page. This is where we would have our news transmission")
